f4600fe5e19f8ce9ba04e287ca87fa2b72c257c2
obwoj1 <149099282+obwoj1@users.noreply.github.com>
2025-03-12 23:38:58 -0400

    Initial Commit

---
+++ Trivia3rd TakeTests/Trivia3rd_TakeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Trivia3rd_TakeTests.swift
+//  Trivia3rd TakeTests
+//
+//  Created by Oba W on 3/12/25.
+//
+
+import Testing
+@testable import Trivia3rd_Take
+
+struct Trivia3rd_TakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
